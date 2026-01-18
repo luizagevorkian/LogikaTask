ec3f2163c173e9c16a253c372b5c335f303370bf
Luiza <66139931+luizagevorkian@users.noreply.github.com>
2026-01-18 15:31:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,34 @@ button.addEventListener('click', () => {
     resultField.innerText = 'До сплати: ' + total + ' грн';
   }
 });
+/*querySelector('calc-btn')
+
+Потрібно використовувати селектор за класом .calc-btn або за id #calc-btn.
+
+'calc-btn' шукає тег <calc-btn>, якого немає.
+
+querySelector(.result)
+
+Рядки в JavaScript завжди беруться в лапки. Має бути '.result'.
+
+addEventListener('click', { ... })
+
+Другий аргумент має бути функція, а не об’єкт. Потрібно ( ) => { ... }.
+
+const quantity = input;
+
+input — це елемент, а не його значення. Потрібно input.value.
+
+І треба перетворити в число: Number(input.value).
+
+Незакритий рядок
+
+'Кількість не може бути менше 0!; має закриватися лапками: 'Кількість не може бути менше 0!'.
+
+Обчислення total
+
+price + quantity буде рядок, якщо quantity не число. Потрібно спочатку перетворити quantity на число.
+
+innerHtml
+
+Правильно пишеться innerHTML або можна використовувати innerText. */
